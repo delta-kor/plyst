4cd7e9ca202d9deb6bff791e75ce075ca57264ec
delta-kor <deltakbot1@gmail.com>
2024-08-11 15:43:20 +0900

    feat: add project config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,69 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './page/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    borderRadius: {
+      0: '0px',
+      4: '4px',
+      8: '8px',
+      16: '16px',
+      full: '9999px',
+    },
+    borderWidth: {
+      1: '1px',
+      2: '2px',
+      3: '3px',
+    },
+    fontSize: {
+      12: ['12px', '14px'],
+      14: ['14px', '17px'],
+      16: ['16px', '19px'],
+      18: ['18px', '21px'],
+      20: ['20px', '24px'],
+      22: ['22px', '26px'],
+      24: ['24px', '29px'],
+      28: ['28px', '33px'],
+      32: ['32px', '38px'],
+      36: ['36px', '43px'],
+    },
+    fontWeight: {
+      400: '400',
+      500: '500',
+      600: '600',
+      700: '700',
+    },
+    spacing: {
+      0: '0',
+      1: '1px',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
+      10: '10px',
+      12: '12px',
+      14: '14px',
+      16: '16px',
+      18: '18px',
+      20: '20px',
+      22: '22px',
+      24: '24px',
+      28: '28px',
+      32: '32px',
+      36: '36px',
+      40: '40px',
+      48: '48px',
+      64: '64px',
+      72: '72px',
     },
   },
-  plugins: [],
 };
 export default config;
